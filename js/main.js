929ca9fac675034b1b89682124822e5594757949
Jittat Fakcharoenphol <jittat@fledged.(none)>
2009-03-11 21:17:49 +0700

    added scale by minutes, taskcount, better handling of velocity input

--- js/main.js
+++ js/main.js
@@ -7,15 +7,22 @@ var velocity = 1;
 var timestamps = null;
 
 function refreshChart() {
-    var maxTime = timestamps[timestamps.length-1];
-    var maxCount = timestamps.length;
+    var chartScale = 1;
+    var lineColor = "00FF00";
 
+    var maxCount = timestamps.length;
+    var maxTime = timestamps[timestamps.length-1];
     if(maxTime==0)
 	maxTime = 1;
 
+    if(maxTime > 120)
+	chartScale = 60;  //  display in minutes
+
     var expectedDone = velocity*maxTime/60;
-    if(expectedDone>maxCount)
+    if(expectedDone>maxCount) {
+	lineColor = "FF0000";
 	maxCount = expectedDone;
+    }
 
     var dataX = '0';
     var dataY = '0';
@@ -27,17 +34,21 @@ function refreshChart() {
 	dataY += ',' + parseInt(count*100 / maxCount);
     });
 
-    var rangeOption = "0,0," + maxTime + "|" +
+    var rangeOption = "0,0," + parseInt(maxTime / chartScale) + "|" +
 	"1,0," + (count+1);
 
     $("#chart").attr("src",
 		     "http://chart.apis.google.com/chart?" +
-		     "chs=300x200&" +
+		     "chs=400x300&" +
 		     "cht=lxy&" + 
 		     "chd=t:" + dataX + "|" + dataY + "|" + 
 		     "0,100|0," + (expectedDone*100/maxCount) + "&" +
 		     "chxt=x,y&" +
-		     "chxr=" + rangeOption);
+		     "chxr=" + rangeOption + "&" +
+		     "chco=" + lineColor + ",707070&" + 
+		     "chls=2,1,0|0.5,1,0");
+
+    $("#done_count").html(count);
 }
 
 function getCurrentTime() {
@@ -50,15 +61,22 @@ function addTimeStamp() {
     timestamps.push(parseInt((getCurrentTime()-startTime)/1000));
 }
 
+function readVelocity() {
+    var v = parseInt($("#velocity").val());
+    if(v==NaN)
+	v = 1;
+    return v;
+}
+
 function initStat() {
-    velocity = $("#velocity").val();
+    velocity = readVelocity();
     startTime = getCurrentTime();
     timestamps = new Array();
 }
 
 function main() {
     $("#change").click(function() {
-	velocity = $("#velocity").val();
+	velocity = readVelocity();
 	refreshChart();
     });
 
